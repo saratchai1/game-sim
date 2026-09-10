ffbfbc1cf9967add721cbecdbdab3e11fbebf95e
saratchai1 <saratchai@gmail.com>
2026-09-10 19:53:09 +0700

    fix: load 3d label compositor hotfix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './three-world.css'
+import './three-world-hotfix.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
