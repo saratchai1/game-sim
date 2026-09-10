0610c8ad1192f81ee4a23a4e14a1da3ce7799381
saratchai1 <saratchai@gmail.com>
2026-09-11 06:10:53 +0700

    feat: bring Mangrove Bay to life

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import LivingWorldOverlay from './LivingWorldOverlay.jsx'
 import './three-world.css'
 import './three-world-hotfix.css'
 import './three-world-natural.css'
 import './living-coast.css'
+import './living-world-overlay.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <LivingWorldOverlay />
   </React.StrictMode>,
 )
