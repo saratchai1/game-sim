41b8802224dbfd00b454a1c3443292b092ec4b05
saratchai1 <saratchai@gmail.com>
2026-09-10 20:01:15 +0700

    style: load the natural coastal world refinements

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './three-world.css'
 import './three-world-hotfix.css'
+import './three-world-natural.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
