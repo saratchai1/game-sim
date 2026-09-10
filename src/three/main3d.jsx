231a075fc9ea24bcc8c0bbacd42b4ae1c038c119
saratchai1 <saratchai@gmail.com>
2026-09-10 20:46:18 +0700

    build: add dedicated 3D application entry

---
+++ src/three/main3d.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App3D from './App3D.jsx'
+import './game3d.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App3D />
+  </React.StrictMode>,
+)
