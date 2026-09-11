75e553bc2bc80ce7f0148f6c79ff6bff22a29168
saratchai1 <saratchai@gmail.com>
2026-09-11 21:53:10 +0700

    feat: populate the coast with village residents

--- src/CoastCharacters.jsx
+++ src/CoastCharacters.jsx
@@ -1,5 +1,10 @@
 import WorkerCharacter from './WorkerCharacter.jsx'
+import VillageLife from './VillageLife.jsx'
 import { workerVariants } from './workerVariants.js'
-export default function CoastCharacters({ action, target }) {
-  return <group>{workerVariants.map((variant,index)=><WorkerCharacter key={variant.id} variant={variant} index={index} action={action} target={target} />)}</group>
+
+export default function CoastCharacters({ action, target, storm=false }) {
+  return <group>
+    {workerVariants.map((variant,index)=><WorkerCharacter key={variant.id} variant={variant} index={index} action={action} target={target} />)}
+    <VillageLife action={action} storm={storm} />
+  </group>
 }
