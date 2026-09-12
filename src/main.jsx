96df34c61630d6725c1b91b0126df51213491bfe
saratchai1 <saratchai@gmail.com>
2026-09-12 12:50:33 +0700

    fix: load landscape plot action override

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './three-world-natural.css'
 import './living-coast.css'
 import './restoration.css'
 import './iphone-safari-fullscreen.css'
+import './iphone-landscape-plant-confirm.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
